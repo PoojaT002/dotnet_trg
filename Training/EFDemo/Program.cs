2d652179ceb9aece88b0fa62000a436dcefcb836
Pooja Tirmal <poojati@infinite.com>
2025-12-12 17:15:46 +0530

    CoddingWeek5

---
+++ Training/EFDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CRUDdemo demo=new CRUDdemo();
+            //demo.SearchRecord();
+            //demo.AddRecord();
+            demo.DeleteRecord();
+
+
+            Console.ReadLine();
+        }
+    }
+}
